254fed30d2191790d40a88a8c56d301ece9d89bf
cxqsb <508951305@qq.com>
2025-03-19 17:05:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 description = """
-自动桥接机器人  https://bridge.t1rn.io/
-操你麻痹Rambeboy,偷私钥🐶
+自动桥接机器人  https://unlock3d.t3rn.io/rewards
+还是继续操你麻痹Rambeboy,偷私钥🐶  V2版本
 """
 
 # 每个链的颜色和符号
@@ -42,11 +42,11 @@ menu_color = '\033[95m'  # 菜单文本颜色
 explorer_urls = {
     'Base': 'https://sepolia.base.org', 
     'OP Sepolia': 'https://sepolia-optimism.etherscan.io/tx/',
-    'BRN': 'https://brn.explorer.caldera.xyz/tx/'
+    'b2n': 'https://b2n.explorer.caldera.xyz/tx/'
 }
 
-# 获取BRN余额的函数
-def get_brn_balance(web3, my_address):
+# 获取b2n余额的函数
+def get_b2n_balance(web3, my_address):
     balance = web3.eth.get_balance(my_address)
     return web3.from_wei(balance, 'ether')
 
@@ -58,7 +58,7 @@ def check_balance(web3, my_address):
 # 创建和发送交易的函数
 def send_bridge_transaction(web3, account, my_address, data, network_name):
     nonce = web3.eth.get_transaction_count(my_address, 'pending')
-    value_in_ether = 0.4
+    value_in_ether = 0.2
     value_in_wei = web3.to_wei(value_in_ether, 'ether')
 
     try:
@@ -74,7 +74,7 @@ def send_bridge_transaction(web3, account, my_address, data, network_name):
         return None
 
     base_fee = web3.eth.get_block('latest')['baseFeePerGas']
-    priority_fee = web3.to_wei(0, 'gwei')
+    priority_fee = web3.to_wei(5, 'gwei')
     max_fee = base_fee + priority_fee
 
     transaction = {
@@ -110,8 +110,8 @@ def send_bridge_transaction(web3, account, my_address, data, network_name):
         print(f"⛽ 使用Gas: {tx_receipt['gasUsed']}")
         print(f"🗳️  区块号: {tx_receipt['blockNumber']}")
         print(f"💰 ETH余额: {formatted_balance} ETH")
-        brn_balance = get_brn_balance(Web3(Web3.HTTPProvider('https://brn.rpc.caldera.xyz/http')), my_address)
-        print(f"🔵 BRN余额: {brn_balance} BRN")
+        b2n_balance = get_b2n_balance(Web3(Web3.HTTPProvider('https://b2n.rpc.caldera.xyz/http')), my_address)
+        print(f"🔵 b2n余额: {b2n_balance} b2n")
         print(f"🔗 区块浏览器链接: {explorer_link}\n{reset_color}")
 
         return web3.to_hex(tx_hash), value_in_ether
@@ -157,8 +157,8 @@ def process_network_transactions(network_name, bridges, chain_data, successful_t
                 print(f"{'='*150}")
                 print("\n")
             
-            # 随机等待 30 到 60 秒
-            wait_time = random.uniform(30, 40)
+            # 随机等待 120 到 180 秒
+            wait_time = random.uniform(60, 80)
             print(f"⏳ 等待 {wait_time:.2f} 秒后继续...\n")
             time.sleep(wait_time)  # 随机延迟时间
 
@@ -180,8 +180,8 @@ def main():
     print("\n\n")
 
     successful_txs = 0
-    current_network = 'Base'  # 默认从 Base 链开始
-    alternate_network = 'OP Sepolia'
+    current_network = 'OP Sepolia'  # 默认从 Base 链开始
+    alternate_network = 'Base'
 
     while True:
         # 检查当前网络余额是否足够
@@ -198,10 +198,10 @@ def main():
         my_address = Account.from_key(private_keys[0]).address  # 使用第一个私钥的地址
         balance = check_balance(web3, my_address)
 
-        # 如果余额不足 0.5 ETH，切换到另一个链
-        if balance < 0.5:
-            print(f"{chain_symbols[current_network]}{current_network}余额不足 0.1 ETH，切换到 {alternate_network}{reset_color}")
-    
+        # 如果余额不足 0.101 ETH，切换到另一个链
+        if balance < 0.2:
+            print(f"{chain_symbols[current_network]}{current_network}余额不足 0.101 ETH，切换到 {alternate_network}{reset_color}")
+            current_network, alternate_network = alternate_network, current_network  # 交换链
 
         # 处理当前链的交易
         successful_txs = process_network_transactions(current_network, ["Base - OP Sepolia"] if current_network == 'Base' else ["OP - Base"], networks[current_network], successful_txs)
